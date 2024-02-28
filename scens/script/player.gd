75cbd5db39f8be8abc48167ec5a91734f47af4fb
Aluno EDU FOR 2 <alunofor2@edu.sesc-ce.com.br>
2024-02-28 09:47:46 -0300

    Adicionando jogo brabo >:)

---
+++ scens/script/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var speed = 200.0
+var combutivel = 100
+var balap = preload("res://scens/lazer.tscn")
+var lado = 1
+
+func _ready():
+	$AnimatedSprite2D.play()
+
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.y = 0
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = velocity.y - speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = velocity.y + speed
+		
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = velocity.x + speed
+		$AnimatedSprite2D.flip_h = false
+		lado = 1
+		
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = velocity.x - speed
+		$AnimatedSprite2D.flip_h = true
+		lado = -1
+	
+	if Input.is_action_just_pressed('atira'):
+		$atira.play()
+		var novabala = balap.instantiate()
+		
+		novabala.position.x = position.x + 4
+		novabala.position.y = position.y + 4
+		novabala.z_index = -1
+		novabala.setLado(lado)
+		get_tree().current_scene.add_child(novabala)
+	
+	move_and_slide()
+	
